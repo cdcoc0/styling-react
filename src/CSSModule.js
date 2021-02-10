90d0c26c6e351a629103c0d033df5c99d18e087c
velykitty <jiyunee13@gmail.com>
2021-02-11 00:16:48 +0900

    classnames -CSS Module 사용시 classnames를 사용해 클래스를 여러 개 설정

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from './CSSModule.module.css';
+
+const cx = classNames.bind(styles); //미리 styles에서 클래스를 받아 오도록 설정
 
 const CSSModule = () => {
     return (
-        <div className={`${styles.wrapper} ${styles.inverted}`}>
+        <div className={cx('wrapper', 'inverted')}>
             Hello, this is <span className="something">CSS Module!</span>
         </div>
     );
