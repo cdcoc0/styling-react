def49bc26c860b70cc1fbfc0b1521c596e91ba70
velykitty <jiyunee13@gmail.com>
2021-02-10 23:33:23 +0900

    CSS Module -tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import SassComponent from './SassComponent';
-import './App.css';
+import CSSModule from './CSSModule';
 
 class App extends Component {
   render() {
     return(
       <diV>
         <SassComponent />
+        <CSSModule />
       </diV>
     );
   }
