d8729ec877180783d3daa9868349d5cb646bba45
velykitty <jiyunee13@gmail.com>
2021-02-17 18:27:16 +0900

    styled-components media query 함수화 : styled-components 매뉴얼의 util 함수 참고

--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768 
+};
+
+//size 객체에 따라 자동으로 media 쿼리 함수 생성
+//참고: http://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css `
+    @media (max-width: ${sizes[label] / 16}em) {
+        ${css(...args)}
+    }`;
+    return acc;
+}, {});
+
 // props로 넣어 준 값을 직접 전달해줄 수 있음
 const Box = styled.div `
     background: ${props => props.color || 'blue'};
@@ -10,12 +25,14 @@ const Box = styled.div `
     //responsive
     width: 1024px;
     margin: 0 auto;
-    @media (max-width: 1024px) {
+    /* @media (max-width: 1024px) {
         width: 768px;
     }
     @media (max-width: 768px) {
         width: 100%
-    }
+    } */
+    ${media.desktop`width: 768;`}
+    ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button `
